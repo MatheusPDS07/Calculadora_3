2f1247a6dce87045e4b920595131c541b5e5ccaa
MatheusPDD07 <matheuspds327@gmail.com>
2022-11-27 08:35:30 -0300

    Initial commit

---
+++ app/src/main/java/com/project/calculadora_3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.project.calculadora_3;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
